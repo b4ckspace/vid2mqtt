08c2eff1bc1f0374c96c59189fc747c4cb19e32d
Stefan Schwarz <stefan@f2o.io>
2019-12-28 17:36:31 +0100

    working

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module vid2mqtt
 
 go 1.13
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+)
--- main.go
+++ main.go
@@ -2,28 +2,41 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"os"
-	"time"
+	"strings"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
+	c := mqtt.NewClient(mqtt.NewClientOptions().AddBroker("tcp://mqtt.iot.exposed:1883"))
 	r := bufio.NewScanner(os.Stdin)
-	r.Split(ScanFrame)
+	r.Split(bufio.ScanRunes)
 
-	for r.Scan() {
-		os.Stdout.Write(r.Bytes())
-		//fmt.Printf("%x", r.Text())
-		time.After(time.Second)
-
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
 	}
-}
 
-func ScanFrame(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	start := 0
-	for ; start < len(data)-4; start++ {
-		if string(data[start:start+4]) == "\033[0m" {
-			return start+5, data[:start+4], nil
+	frame := bytes.NewBufferString("")
+	for r.Scan() {
+		_, _ = frame.Write(r.Bytes())
+		if strings.HasSuffix(frame.String(), "\033[0") {
+			os.Stdout.Write(frame.Bytes())
+			os.Stdout.Sync()
+			token := c.Publish("text", 0, false, "\r" + frame.String())
+			token.Wait()
+			frame = bytes.NewBufferString("")
 		}
 	}
-	return start, nil, nil
 }
+
+//func ScanFrame(data []byte, atEOF bool) (advance int, token []byte, err error) {
+//	start := 0
+//	for ; start < len(data)-4; start++ {
+//		if string(data[start:start+4]) == "\033[0m" {
+//			return start + 4, data[:start+4], nil
+//		}
+//	}
+//	return start, nil, nil
+//}
