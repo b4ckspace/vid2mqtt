41e18cf0bbe8f4eb86ce2daab5ade5b16b6b4d3c
Stefan Schwarz <stefan@f2o.io>
2019-12-28 19:15:33 +0100

    performance

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"os"
-	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -18,25 +17,18 @@ func main() {
 		panic(token.Error())
 	}
 
+	token := make([]byte, 3)
 	frame := bytes.NewBufferString("")
 	for r.Scan() {
-		_, _ = frame.Write(r.Bytes())
-		if strings.HasSuffix(frame.String(), "\033[0") {
-			os.Stdout.Write(frame.Bytes())
+		b := r.Bytes()
+		_, _ = frame.Write(b)
+		token = append(token[1:3], b...)
+		if string(token) == "\033[0" {
+			os.Stdout.WriteString("\r" + frame.String())
 			os.Stdout.Sync()
-			token := c.Publish("text", 0, false, "\r" + frame.String())
+			token := c.Publish("text", 0, false, "\r"+frame.String())
 			token.Wait()
-			frame = bytes.NewBufferString("")
+			frame.Reset()
 		}
 	}
 }
-
-//func ScanFrame(data []byte, atEOF bool) (advance int, token []byte, err error) {
-//	start := 0
-//	for ; start < len(data)-4; start++ {
-//		if string(data[start:start+4]) == "\033[0m" {
-//			return start + 4, data[:start+4], nil
-//		}
-//	}
-//	return start, nil, nil
-//}
