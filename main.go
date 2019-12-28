03733904248931ff89bbd62fcb872b5177e8bda1
Stefan Schwarz <stefan@f2o.io>
2019-12-28 19:33:06 +0100

    purrformance

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"bytes"
+	"log"
 	"os"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -10,27 +10,30 @@ import (
 
 func main() {
 	c := mqtt.NewClient(mqtt.NewClientOptions().AddBroker("tcp://mqtt.iot.exposed:1883"))
-	r := bufio.NewScanner(os.Stdin)
-	r.Split(bufio.ScanRunes)
-
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+		log.Fatalf("Mqtt error: %s", token.Error())
 	}
 
-	token := make([]byte, 3)
-	frame := bytes.NewBufferString("")
-	for r.Scan() {
-		b := r.Bytes()
-		_, _ = frame.Write(b)
-		token[0] = token[1]
-		token[1] = token[2]
-		token[2] = b[0]
-		if string(token) == "\033[0" {
-			os.Stdout.WriteString("\r" + frame.String())
-			os.Stdout.Sync()
-			token := c.Publish("text", 0, false, "\r"+frame.String())
-			token.Wait()
-			frame.Reset()
+	s := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 0, 64*1024)
+	s.Buffer(buf, 1024*1024)
+
+	for s.Scan() {
+		text := s.Text()
+		os.Stdout.WriteString(text)
+		token := c.Publish("text", 0, false, "\r"+text)
+		token.Wait()
+	}
+
+	log.Fatalf("Scan aborted: %s", s.Err())
+}
+
+func ScanFrame(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	start := 0
+	for ; start < len(data)-3; start++ {
+		if string(data[start:start+3]) == "\033[0" {
+			return start + 3, data[:start+3], nil
 		}
 	}
+	return 0, nil, nil
 }
